b4475424801df424e933f0e8fc3d478778f47c8b
mecaniser <35816502+mecaniser@users.noreply.github.com>
2018-04-14 12:24:44 -0400

    Homepage.js final touch Version 2.1

--- public/assets/js/homepage2.js
+++ public/assets/js/homepage2.js
@@ -23,17 +23,12 @@ $(document).ready(function () {
         console.log(response);
   
         var catCarousel = $("#catCarousel");
-        // var carousel = document.getElementById("catCarousel");
-        // let defaultOption = document.createElement("a");
-        // carousel.appendChild(defaultOption);
-        // .replace("public/", "")
   
         for (var i = 0; i < response.length; i++) {
           console.log(response[i]);
   
           var carouselItem = $("<a>");
           carouselItem.addClass("carousel-item");
-          // carouselItem.attr("href", "#");
           carouselItem.text(response[i].breed);
   
           var carouselImage = $("<img>");
@@ -41,22 +36,12 @@ $(document).ready(function () {
           carouselImage.attr("alt", response[i].breed);
           carouselImage.attr("value", response[i].id);
           carouselImage.addClass("cat-image", response[i].id);
-          // carouselImage.attr("data-caption", response[i].breed);
-  
-          // carouselImage.attr("class", "tooltipped");
-          // carouselImage.attr("data-position", "top");
-          // carouselImage.attr("data-tooltip", response[i].breed);
-         
-  
           carouselImage.appendTo(carouselItem);
           carouselItem.appendTo(catCarousel);
         }
         
         $(".carousel").carousel();
-        // $('.tooltipped').tooltip();
-      
       });
-     
     }
   
     populateCats();
@@ -64,7 +49,6 @@ $(document).ready(function () {
     $(document).on("click", ".cat-image", function () {
       $(".catLoad").show();
   
-      // var catId = window.location.search.split("=")[1];
       var catId = $(this).attr("value");
       console.log("cat id: " + catId);
   
@@ -85,16 +69,7 @@ $(document).ready(function () {
         $(".health").html(response.health);
         $(".care").html(response.care);
         $(".children_animals").html(response.children_animals);
-  
-        // $(".lifeSpan-holder").html("<h5>Life Span: </h5>" + response.life_span + "<br>");
-        // $(".weight-holder").html("<h5>Weight: </h5>"+ response.weight + "<br>");
       });
-  
-      // $(".slider").slider();
-      // $(".collapsible").collapsible();
-      // //
-      // $('.carousel').carousel();
-      // //
       $.ajax({
         url: "/api/comments/" + catId,
         method: "GET"
@@ -112,11 +87,6 @@ $(document).ready(function () {
             console.log(dateString)
             newPost.html("<p>" + dateString + "<p><p>" + response[i].name + "<p>" + "<p>" + response[i].comment + "</p> <hr>");
             $(".comment-container").append(newPost)
-            // test = $(".comment-container").val()
-            // if (test === "") {
-            //   $(".comment-container").html("No comments have been posted for this breed.")
-            // }
-            
           }
         }
       }); 
@@ -129,7 +99,6 @@ $(document).ready(function () {
       var instance = M.Collapsible.init(elem, {
         accordion: false
       }); // Close accordion Materialize function
-  
     }); // Close click handler/AJAX call for cat info
   }); // Close document.ready
   
@@ -159,7 +128,7 @@ $(document).ready(function () {
   
       $("select").formSelect();
     });
-  }
+  };
   
   populateBreed();
   
@@ -174,7 +143,7 @@ $(document).ready(function () {
     // Wont submit the post if we are missing a body or a title
     if (!nameInput.val().trim() || !commentInput.val().trim() || !postIdSelect.val()) {
       return;
-    }
+    };
     // Constructing a newPost object to hand to the database
     var newPost = {
       name: nameInput.val().trim(),
@@ -208,14 +177,10 @@ $(document).ready(function () {
           $(".comment-container").append(newPost)    
         }
       }
-      // $("#name").text("");
-      // $("#comment").text("");
     });
-     
-  }) 
+  }); 
   
   function submitPost(Post) {
     $.post("/api/comments/", Post, function () {
-      // window.location.href = "/";
     });
-  }
+  };
