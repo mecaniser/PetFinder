a0be28b9156fcfbc66d41091b773c7ce785b9a80
mecaniser <35816502+mecaniser@users.noreply.github.com>
2018-04-14 23:10:34 -0400

    Heroku deploy

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,9 +4,6 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-//   app.get("/cms", function(req, res) {
-//     res.sendFile(path.join(__dirname, "../public/cms.html"));
-//   });
 
   app.get("/comment", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/comment.html"));
